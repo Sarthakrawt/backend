5ed42f457662a4df5e659b3be42c2bf56a0f2c3a
Sarthakrawt <155871591+Sarthakrawt@users.noreply.github.com>
2024-11-28 08:14:03 -0500

    changed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,11 +2,31 @@ import { v2 as cloudinary } from 'cloudinary';
 //nodejs fs
 // file  read write remove from you local memory 
 // we can use unlink to remove the file from the server 
+import fs from "fs";
+// fs is a file system 
  
     cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
         api_secret: process.env.CLOUDINARY_API_SECRET
     });
-    
+
+    const uploadOnCloudinary = async(localFilePath) => {
+        try{
+           if(!localFilePath) return null
+           //upload the file on cloudinary 
+           const response = await  cloudinary.uploader.upload(localFilePath, {
+          resource_type: "auto"
+           })
+           // file has been uploaded successfully
+           console.log("file is uploaded on cloudinary", response.url)
+           return response
+        }catch(error){
+          fs.unlinkSync(localFilePath)
+          // this code remove the locally saved temporary file as the upload operation god failed
+          return null;
+        }
+    }
+
+export {uploadOnCloudinary};
     
